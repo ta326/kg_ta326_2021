f0329779aec588337e751fc378867d746911d5d9
ta326 <ta326@cornell.edu>
2020-03-15 22:01:29 -0400

    modified python code to account for using comman arguement

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+
+# Module sys for importing arguement
+import sys   
+a = sys.argv[1]
+b = sys.argv[2]
+
 # this  function prints true, if string a can be mmapped to string b or else it prints false
+# Precondition: mapping has two arguements
 def mapping(a, b):
-    # if strings do not have same length mapping is not possible
-    if len(a) != len(b):
+    # if a has a smaller length, mapping of all character of a is not possible
+    if len(a) > len(b):
         print("false")
         return
+    Dict = {}
     for i in range(0,len(a)):
         if a[i] not in Dict:
             Dict.update({a[i] : b[i]})  
@@ -11,3 +19,6 @@ def mapping(a, b):
             print("false")
             return
     print("true")
+    
+# calling the function using the aruguements    
+mapping(a,b)
